e0274faca930cba1794c4d174548a175ef2489c7
Davide Rombolà <davide.rombola@gmail.com>
2014-04-09 20:55:04 +0200

    sleepy_plug: code review clean up refer to this commit https://github.com/faux123/Nexus_5/commit/3ea0309dd281a3eda4c962217d45c1c858c79449

--- arch/arm/mach-msm/sleepy_plug.c
+++ arch/arm/mach-msm/sleepy_plug.c
@@ -33,7 +33,7 @@
 #undef DEBUG_SLEEPY_PLUG
 
 #define SLEEPY_PLUG_MAJOR_VERSION	1
-#define SLEEPY_PLUG_MINOR_VERSION	5
+#define SLEEPY_PLUG_MINOR_VERSION	6
 
 #define DEF_SAMPLING_MS			(1000)
 #define BUSY_SAMPLING_MS		(500)
@@ -59,7 +59,7 @@ enum mp_decisions {
 static unsigned int sleepy_plug_active = 1;
 module_param(sleepy_plug_active, uint, 0644);
 
-static unsigned int sampling_time = 0;
+static unsigned int sampling_time = DEF_SAMPLING_MS;
 static bool suspended = false;
 static unsigned int rq_values[RQ_VALUE_ARRAY_DIM] = {6};
 
@@ -175,31 +175,11 @@ static void sleepy_plug_input_event(struct input_handle *handle,
                 msecs_to_jiffies(10));
 }
 
-static int input_dev_filter(const char *input_dev_name)
-{
-	if (strstr(input_dev_name, "touchscreen") ||
-		strstr(input_dev_name, "sec_touchscreen") ||
-		strstr(input_dev_name, "touch_dev") ||
-		strstr(input_dev_name, "atmel_mxt224e") ||
-		strstr(input_dev_name, "-keypad") ||
-		strstr(input_dev_name, "-nav") ||
-		strstr(input_dev_name, "-oj")) {
-		pr_info("touch dev: %s\n", input_dev_name);
-		return 0;
-	} else {
-		pr_info("touch dev: %s\n", input_dev_name);
-		return 1;
-	}
-}
-
 static int sleepy_plug_input_connect(struct input_handler *handler,
 		struct input_dev *dev, const struct input_device_id *id)
 {
 	struct input_handle *handle;
 	int error;
-
-	if (input_dev_filter(dev->name))
-		return -ENODEV;
 
 	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
 	if (!handle)
@@ -233,7 +213,21 @@ static void sleepy_plug_input_disconnect(struct input_handle *handle)
 }
 
 static const struct input_device_id sleepy_plug_ids[] = {
-	{ .driver_info = 1 },
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_EVBIT |
+			 INPUT_DEVICE_ID_MATCH_ABSBIT,
+		.evbit = { BIT_MASK(EV_ABS) },
+		.absbit = { [BIT_WORD(ABS_MT_POSITION_X)] =
+			    BIT_MASK(ABS_MT_POSITION_X) |
+			    BIT_MASK(ABS_MT_POSITION_Y) },
+	}, /* multi-touch touchscreen */
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_KEYBIT |
+			 INPUT_DEVICE_ID_MATCH_ABSBIT,
+		.keybit = { [BIT_WORD(BTN_TOUCH)] = BIT_MASK(BTN_TOUCH) },
+		.absbit = { [BIT_WORD(ABS_X)] =
+			    BIT_MASK(ABS_X) | BIT_MASK(ABS_Y) },
+	}, /* touchpad */
 	{ },
 };
 
